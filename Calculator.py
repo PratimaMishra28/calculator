6735992eeee4995ee8a7a0846b62320b3d853595
pratima <022pratimamishra@gmail.com>
2025-06-15 16:38:39 +0530

    Added main and calculator file

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+
+class Calculator:
+    def addition(self, num1, num2):
+        num3 = num1 + num2
+        return num3
+
+    def subtraction(self, num1, num2):
+        num3 = num1 - num2
+        return num3
+
+    def multiplication(self, num1, num2):
+        num3 = num1 * num2
+        return num3
+
+    def division(self, num1, num2):
+        num3 = num1 / num2
+        return num3
+
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+from Calculator import Calculator
+
+if __name__ == '__main__':
+
+
+    calc = Calculator()
+
+    # Call methods
+    print("Addition:", calc.addition(10, 5))
+    print("Subtraction:", calc.subtraction(10, 5))
+    print("Multiplication:", calc.multiplication(10, 5))
+    print("Division:", calc.division(10, 5))
+
+
+
